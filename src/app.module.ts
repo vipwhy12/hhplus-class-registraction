9f22b474c97de5033815d74bb4c175e91f281f2d
vipwhy12 <vipwhy12@naver.com>
2024-10-01 00:30:30 +0900

    feat: TypeOrm 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'LectureReservation',
+      autoLoadEntities: true,
+      synchronize: true,
+      logging: true,
+      dropSchema: true,
+      entities: [],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
