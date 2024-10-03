430759a3e486e6895e4076b113df166a32f91186
vipwhy12 <vipwhy12@naver.com>
2024-10-03 13:02:06 +0900

    refactor: 수강 가능한 목록 조회 함수 명 변경, 반환 타입 추가

--- src/lecture/lecture.repository.ts
+++ src/lecture/lecture.repository.ts
@@ -1,6 +1,7 @@
+import { LectureOption } from './entity/lecture.option.entity';
+
 export const LECTURE_REPOSITORY = Symbol('LECTURE_REPOSITORY');
 
 export interface LectureRepository {
-  // findAvailableLecturesByDate(date: Date): Promise<lectureOptionRepository[]>;
-  findAvailableLecturesByDate(date: Date);
+  findAvailableLecturesByDate(date: Date): Promise<LectureOption[]>;
 }
--- src/lecture/lecture.service.ts
+++ src/lecture/lecture.service.ts
@@ -1,7 +1,6 @@
 import { LectureMapper } from './lecture.mapper';
 import { Inject, Injectable } from '@nestjs/common';
 import { LectureListDto } from './dto/lecutre.list.dto';
-import { LectureOption } from './entity/lecture.option.entity';
 import { LectureRepository, LECTURE_REPOSITORY } from './lecture.repository';
 
 @Injectable()
@@ -13,13 +12,9 @@ export class LectureService {
   ) {}
 
   //날짜별로 현재 신청 가능한 특강 목록을 조회하는 API
-  async getListByDate(date: Date): Promise<LectureListDto[]> {
-    const lectures = await this.getLecturesByDate(date);
+  async findAvailableLectures(date: Date): Promise<LectureListDto[]> {
+    const lectures =
+      await this.lectureRepository.findAvailableLecturesByDate(date);
     return this.lectureMapper.toLectureListDto(lectures);
-  }
-
-  //날짜별로 최대 관중 수에 맞는 특강을 조회
-  async getLecturesByDate(date: Date): Promise<LectureOption[]> {
-    return await this.lectureRepository.findAvailableLecturesByDate(date);
   }
 }
