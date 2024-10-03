4d10b4cac8b1ff6b729fb2b80ea4709730a4a6a2
vipwhy12 <vipwhy12@naver.com>
2024-10-04 02:24:09 +0900

    refactor: 특강 신청 api URL 변경

--- src/lecture-facade/lecture-facade.controller.ts
+++ src/lecture-facade/lecture-facade.controller.ts
@@ -2,12 +2,12 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { LectureFacadeService } from './lecture-facade.service';
 import { ApplyForLectureDto } from './dto/apply.for.lecture.dto';
 
-@Controller('lecture')
+@Controller('reservation')
 export class LectureFacadeController {
   constructor(private readonly lectureFacadeService: LectureFacadeService) {}
 
   // 1️⃣ 특강 신청 API: 특정 일자를 기준으로 청강할 수 있는 특강 목록 조회
-  @Post('apply')
+  @Post()
   async applyForLecture(@Body() applyForLectureDto: ApplyForLectureDto) {
     const { lectureOptionId, userId } = applyForLectureDto;
     return await this.lectureFacadeService.applyForLecture(
